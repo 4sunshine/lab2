40b092fb742b5159b5bfb6904c9af90badd7f5ec
4sunshine <alexandershain.5@gmail.com>
2019-12-20 19:09:24 +0300

    Create HTML elements added

--- prepare.js
+++ prepare.js
@@ -1,3 +1,39 @@
-function f() {
-        document.getElementById("wb").innerHTML = "LOL";
+function requestForFilenames() {
+
+        var xreq = new XMLHttpRequest();
+
+        xreq.open('GET','/works',false);
+
+        xreq.send();
+
+        document.getElementById("wb").style.display="none";
+
+        document.getElementById("uploader").style.display="none";
+
+        var data = JSON.parse(xreq.responseText);
+
+        addContent(data);
+}
+
+function addContent(data) {
+        var ul = document.createElement("ul");
+
+        ul.innerHTML = `<ul id="worklists"></ul>`;
+
+        document.getElementById("task").appendChild(ul);
+
+        let i = 0;
+
+        data.forEach((element) => {
+
+                var li = document.createElement("li");
+
+                li.innerHTML = `<li id="work${i}">${element.Filename}</li>`;
+
+                i++;
+
+                ul.appendChild(li);
+
+                document.getElementById("worklists").appendChild(li);
+        });
 }
