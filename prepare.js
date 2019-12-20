b5acaa0cdcfae0d0ed006db8b5e0c7e9218ee535
4sunshine <alexandershain.5@gmail.com>
2019-12-20 20:03:18 +0300

    Radiobutton added

--- prepare.js
+++ prepare.js
@@ -6,23 +6,52 @@ function requestForFilenames() {
 
         xreq.send();
 
-        document.getElementById("wb").style.display="none";
-
-        document.getElementById("uploader").style.display="none";
-
         var data = JSON.parse(xreq.responseText);
 
-        addContent(data);
+        if (data) {
+                document.getElementById("wb").style.display = "none";
+
+                document.getElementById("uploader").style.display= "none";
+
+                addContent(data);
+        }
+        else {
+                alert('Работы отсутствуют!');
+        }
+
 }
 
 function addContent(data) {
+
+        let i = 0;
+
+        var div = document.createElement("div");
+
+        div.innerHTML = '<div class="container" id="radiodiv"></div>';
+
+        document.getElementById("task").appendChild(div);
+
+        data.forEach((element) => {
+                var work = document.createElement("p");
+
+                work.innerHTML =
+                    `<p><input type="radio" name="workname" id="work${i}"\
+                        value="${element.Filename}"> ${element.Filename}</p>`;
+
+                div.appendChild(work);
+        });
+/*
+        var inform = `
+        
+        `;
+
         var ul = document.createElement("ul");
 
         ul.innerHTML = `<ul id="worklists"></ul>`;
 
         document.getElementById("task").appendChild(ul);
 
-        let i = 0;
+        //let i = 0;
 
         data.forEach((element) => {
 
@@ -36,4 +65,6 @@ function addContent(data) {
 
                 document.getElementById("worklists").appendChild(li);
         });
+
+ */
 }
